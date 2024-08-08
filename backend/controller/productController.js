4fcbf615c013c2f920891b01aafc2feabafad6d5
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2024-08-08 09:04:22 +0530

    product all api completed

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -2,6 +2,7 @@
 
 const Product = require("../models/product.model");
 const uploadOnCloudinary = require("../utils/cloudinaryConfig");
+const mongoose = require("mongoose");
 
 exports.createProduct = async (req, res) => {
   try {
@@ -67,5 +68,126 @@ exports.createProduct = async (req, res) => {
     res.status(500).json({
       message: "Server error. Please try again later.",
     });
+  }
+};
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({
+      success: true,
+      data: products,
+    });
+  } catch (error) {
+    console.error("Get All Products Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error. Please try again later.",
+    });
+  }
+};
+
+exports.getSingleProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).json({ message: "Invalid Product Id" });
+    }
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).json({ message: "Invalid Product Id" });
+    }
+    const {
+      name,
+      discription,
+      price,
+      discountprice,
+      category,
+      brand,
+      stock,
+      sku,
+      variants,
+      returnPolilcy,
+      seller,
+    } = req.body;
+    const imagePath = req?.file?.path;
+    let imageUrl;
+    if (imagePath) {
+      const images = uploadOnCloudinary(imagePath);
+      if (!images || images.url) {
+        return res.status(400).json({ message: "Failed to upload image" });
+      }
+      imageUrl = images.url;
+    }
+
+    let updateFiled = {
+      name,
+      discription,
+      price,
+      discountprice,
+      category,
+      brand,
+      stock,
+      sku,
+      variants,
+      returnPolilcy,
+      seller,
+    };
+
+    if (imageUrl) {
+      updateFiled.images = imageUrl;
+    }
+
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      updateFiled,
+      { new: true }
+    );
+    res.status(200).json({
+      message: "Product updated successfully",
+      date: product,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).json({ message: "Invalid Product Id" });
+    }
+    const product = await Product.findByIdAndDelete(productId);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,27 @@
 // backend/routes/productRoutes.js
-
 const express = require("express");
 const { upload } = require("../middleware/uploadFileMiddleware");
-const { createProduct } = require("../controller/productController");
+const {
+  createProduct,
+  updateProduct,
+  getAllProducts,
+  getSingleProduct,
+  deleteProduct,
+} = require("../controller/productController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 // Route to create a new product
-router.post("/create-product", upload.single("images"), createProduct);
+router.post("/create-product",authMiddleware, upload.single("images"), createProduct);
+router.get("/get-all-product", getAllProducts);
+router.get("/get-single-product/:id", authMiddleware, getSingleProduct);
+router.put(
+  "/update-product/:id",
+  authMiddleware,
+  upload.single("images"),
+  updateProduct
+);
+router.delete("/delete-product/:id", authMiddleware, deleteProduct);
 
 module.exports = router;
